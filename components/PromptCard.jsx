16491f65f35608803061f1440f5bcccf441c2f7f
codebyrobin <codebyrobin@gmail.com>
2023-05-20 23:33:44 +0100

    created route for prompts

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -6,6 +6,10 @@ import { useSession } from "next-auth/react";
 import { usePathName, useRouter } from "next/navigation";
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
+  const { data: session } = useSession();
+  const pathName = usePathName;
+  const router = useRouter;
+
   const [copied, setCopied] = useState("");
   const handleCopy = () => {
     setCopied(post.prompt);
@@ -52,6 +56,23 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
       >
         {post.tag}
       </p>
+
+      {session?.user.id === post.creator.id && pathName === "/profile" && (
+        <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
+          <p
+            className="font-inter text-sm green_gradient cursor-pointer"
+            onClick={handleEdit}
+          >
+            Edit
+          </p>
+          <p
+            className="font-inter text-sm green_gradient cursor-pointer"
+            onClick={handleDelete}
+          >
+            Delete{" "}
+          </p>
+        </div>
+      )}
     </div>
   );
 };
