2f98e4aadeb39e6a7d09cd64ac1e27b17637ef43
codebyrobin <codebyrobin@gmail.com>
2023-05-21 18:23:21 +0100

    CRUD functionality

--- app/api/profile/page.jsx
+++ app/api/profile/page.jsx
@@ -5,9 +5,11 @@ import { useSession } from "next-auth/react";
 import { useRouter } from "next-navigation";
 
 import Profile from "@components/profile";
+import { Router } from "next/router";
 
 const MyProfile = () => {
   const { data: session } = useSession();
+  const router = useRouter();
 
   const [posts, setPosts] = useState([]);
   useEffect(() => {
@@ -20,9 +22,26 @@ const MyProfile = () => {
       if (session?.user.id) fetchPosts();
     };
   }, []);
-  const handleEdit = () => {};
+  const handleEdit = (post) => {
+    router.push(`/update-prompt?id=${post._id}`);
+  };
 
-  const handleDelete = async () => {};
+  const handleDelete = async (post) => {
+    const hasConfirmed = confirm(
+      "Are you sure you want to delete this prompt?"
+      if(hasConfirmed)
+ {
+  try {
+    await fetch(`/api/prompt/${post._id.toString()}`, {
+      method: 'DELETE'
+    });
+
+    const filteredPosts = posts.filter((p) => p._id !== post._id);
+  } catch(error) {
+    console.log(error);
+  }
+ }    );
+  };
 
   return (
     <Profile
